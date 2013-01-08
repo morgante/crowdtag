cc9115131f86b8f6addae07a13df788fee53ca54
Morgante Pell <morgante.pell@morgante.net>
2013-01-08 00:10:48 -0500

    handler for form

--- crowdtag.plugin.php
+++ crowdtag.plugin.php
@@ -8,20 +8,31 @@ class CrowdTag extends Plugin
 	}
 	
 	public function theme_collect_tags( $theme )
-	{
+	{		
 		// assign placeholder text
-		$theme->form = self::form();
+		$theme->form = self::form( $theme->post );
 		
 		// Return the output of the custom template
 		return $theme->fetch( 'tag_collection' );
 	}
 	
-	private static function form()
+	private static function form( $post )
 	{
 		$form = new FormUI( 'tag_collection' );
+		
 		$form->append( 'text', 'tag_suggestions', 'null:null', _t('Suggest tags for this post') );
 		$form->append('submit', 'save', _t('Submit'));
+		
+		// register callback
+		$form->on_success( 'CrowdTag::handle_form', $post );
+		
 		return $form;
+	}
+	
+	public static function handle_form( $form, $post )
+	{
+		Utils::debug( $post );
+		exit;
 	}
 }
 ?>
